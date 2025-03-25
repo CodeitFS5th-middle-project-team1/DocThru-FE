2f41691841b97ee9ae50d33d590463d2f2f8bc4b
HEES56 <rlagmltjd125@gmail.com>
2025-03-25 18:37:51 +0900

    공통 컴포넌트 버튼 및 탭(메뉴)구현 완료

---
+++ src/stories/Tab.stories.ts
@@ -0,0 +1,38 @@
+import {
+  Tab,
+  TabActive,
+  TextPosition,
+} from '@/shared/components/tab/TabTopBtn';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Tab',
+  component: Tab,
+  argTypes: {
+    position: {
+      control: 'select',
+      options: Object.values(TextPosition),
+    },
+    isActive: {
+      control: 'select',
+      options: Object.values(TabActive),
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Tab>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const chip: Story = {
+  args: {
+    position: TextPosition.MIDDLE,
+    isActive: TabActive.ON,
+    onClick: () => {},
+    children: '참여중인 챌린지',
+  },
+};
