d63491db7d6d7499f326605369c051950e7bb310
김조순 <whtns611@naver.com>
2025-03-31 09:59:18 +0900

    Fix : header Divider 추가

--- src/shared/components/layout/Header.tsx
+++ src/shared/components/layout/Header.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Member from '@/shared/Img/profile-icon/member.svg';
 import admin from '@/shared/Img/profile-icon/admin.svg';
 import BassBell from '@/shared/Img/bell-icon/bass.svg';
+import { Divider } from '../Divider';
 
 interface HeaderProps {
   isLogin: string | null;
@@ -20,39 +21,42 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   const ImageStyle = 'cursor-pointer';
   return (
-    <div className="max-w-[1200px] w-full h-15 bg-white flex items-center justify-between md:px-6 px-4  lg:py-[14px]">
-      <Image
-        className={ImageStyle}
-        width={120}
-        height={27}
-        src={Logo}
-        alt="logo"
-      />
-      {!isLogin ? (
-        <button
-          onClick={onLogin}
-          className="px-6 py-[10px] cursor-pointer border-solid border-[1px] rounded-xl"
-        >
-          로그인
-        </button>
-      ) : role !== 'admin' ? (
-        <div className="flex items-center gap-4">
-          <Image
-            className={ImageStyle}
-            src={Member}
-            alt="member"
-            onClick={onLogout}
-          />
-          <Image className={ImageStyle} src={BassBell} alt="bell" />
-        </div>
-      ) : (
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className="max-w-[1200px] w-full h-15 bg-white flex items-center justify-between md:px-6 px-4  lg:py-[14px]">
         <Image
           className={ImageStyle}
-          src={admin}
-          alt="admin"
-          onClick={onLogout}
+          width={120}
+          height={27}
+          src={Logo}
+          alt="logo"
         />
-      )}
+        {!isLogin ? (
+          <button
+            onClick={onLogin}
+            className="px-6 py-[10px] cursor-pointer border-solid border-[1px] rounded-xl"
+          >
+            로그인
+          </button>
+        ) : role !== 'admin' ? (
+          <div className="flex items-center gap-4">
+            <Image
+              className={ImageStyle}
+              src={Member}
+              alt="member"
+              onClick={onLogout}
+            />
+            <Image className={ImageStyle} src={BassBell} alt="bell" />
+          </div>
+        ) : (
+          <Image
+            className={ImageStyle}
+            src={admin}
+            alt="admin"
+            onClick={onLogout}
+          />
+        )}
+      </div>
+      <Divider />
     </div>
   );
 };
