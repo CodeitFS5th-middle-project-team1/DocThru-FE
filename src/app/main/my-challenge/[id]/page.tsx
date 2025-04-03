3144d4410e027328a11e95efa45695aedff5d03d
YOONMINHO <ujm4243@naver.com>
2025-04-03 09:32:54 +0900

    feat: 나의챌린지페이지 상세페이지 구현 완료

--- src/app/main/my-challenge/[id]/page.tsx
+++ src/app/main/my-challenge/[id]/page.tsx
@@ -1,7 +1,26 @@
-import { NextPage } from "next";
+import { getDetail } from './components/mock';
+import { MiniCard } from './components/cardDetail';
+import Status from './components/status';
 
-const MyChallengeDetail: NextPage = () => {
-  return <></>;
-};
+interface Props {
+  params: { id: string };
+}
 
-export default MyChallengeDetail;
+export default async function PostDetailPage({ params }: Props) {
+  const data = await getDetail(params.id);
+
+  return (
+    <div>
+      <Status
+        status={data.approvalStatus}
+        reason={data.approvalReason}
+        name={data.name}
+        time={data.time}
+      />
+      <MiniCard info={data} />
+      <div>
+        <p>원문 링크</p>
+      </div>
+    </div>
+  );
+}
