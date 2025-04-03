d0ce751fac261bd04358c12f8fe074b8abc7965d
hyuk-dev <hyuk.development@gmail.com>
2025-04-03 09:39:09 +0900

    feat: 페이지 기본 세팅

--- src/app/main/translation-work/page.tsx
+++ src/app/main/translation-work/page.tsx
@@ -1,6 +1,26 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Logo from '@/shared/Img/logo.svg';
+import Button from '@/shared/components/button/Button';
 
 const TranslationWork: NextPage = () => {
-  return <></>;
+  return (
+    <div className="max-w-[1000px] w-full h-full">
+      <div className="mt-6">
+        <div>
+          <Image
+            className={'cursor-pointer'}
+            width={120}
+            height={27}
+            src={Logo}
+            alt="logo"
+          />
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
 };
 export default TranslationWork;
