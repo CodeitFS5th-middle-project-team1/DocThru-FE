0d2a9a8f653e6b50e5f8c93907b6273560c6729b
hyuk-dev <hyuk.development@gmail.com>
2025-04-03 13:45:07 +0900

    feat: 모달 작업

--- src/app/main/translation-work/page.tsx
+++ src/app/main/translation-work/page.tsx
@@ -3,12 +3,20 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import Logo from '@/shared/Img/logo.svg';
+import Close from '@/shared/Img/close-icon/close.svg';
 import Button, {
   BGColor,
   ButtonBorder,
 } from '@/shared/components/button/Button';
+import { useState } from 'react';
+import ConfirmCancel from '@/shared/components/modal/confirmCancel';
+import Navigate from '@/shared/components/modal/navigate';
 
 const TranslationWork: NextPage = () => {
+  const [isDrafted, setIsDrafted] = useState(true);
+  const [isDraftModal, setIsDraftModal] = useState(false);
+  const [isForgiveModal, setIsForgiveModal] = useState(false);
+  const [isSuccessModal, setIsSuccessModal] = useState(false);
   return (
     <div className="max-w-[1000px] w-full h-full">
       <div className="mt-6 flex justify-between h-[80px] items-center">
@@ -28,7 +36,7 @@ const TranslationWork: NextPage = () => {
               bgColor={BGColor.RED}
               closeIcon={true}
               onClick={() => {
-                confirm('정말로 포기하시겠습니까?');
+                setIsForgiveModal(true)
               }}
             >
               포기
@@ -39,14 +47,20 @@ const TranslationWork: NextPage = () => {
               border={ButtonBorder.RECTANGLE_BORDER}
               bgColor={BGColor.WHITE}
               onClick={() => {
-                alert('임시로 저장되었습니다!');
+                setIsDraftModal(true)
               }}
             >
               임시저장
             </Button>
           </div>
           <div className="w-[90px] h-[40px]">
-            <Button border={ButtonBorder.RECTANGLE} bgColor={BGColor.BLACK} onClick={() => {alert("제출 되었습니다!")}}>
+            <Button
+              border={ButtonBorder.RECTANGLE}
+              bgColor={BGColor.BLACK}
+              onClick={() => {
+                setIsSuccessModal(true)
+              }}
+            >
               제출하기
             </Button>
           </div>
@@ -57,8 +71,68 @@ const TranslationWork: NextPage = () => {
           <input className="text-[20px] w-full" />
         </div>
         <hr />
-        <div className="mt-5">에디터</div>
+        <div className="mt-5">
+          에디터 부분
+        </div>
       </div>
+      <ConfirmCancel
+        isOpen={isDraftModal}
+        onClose={() => setIsDraftModal(false)}
+        onConfirm={() => {
+          setIsDraftModal(false);
+          setIsDrafted(false);
+        }}
+        onCancel={() => setIsDraftModal(false)}
+      >
+        이전 작업물을 불러오시겠어요?
+      </ConfirmCancel>
+      <ConfirmCancel
+        isOpen={isForgiveModal}
+        onClose={() => setIsForgiveModal(false)}
+        onConfirm={() => {
+          setIsForgiveModal(false);
+        }}
+        onCancel={() => setIsForgiveModal(false)}
+      >
+        정말 작업을 포기하시겠어요?
+      </ConfirmCancel>
+      <Navigate
+        isOpen={isSuccessModal}
+        onClose={() => {}}
+        navigateUrl='/main'
+        text='작업물 보기'
+      >
+        제출되었습니다!
+      </Navigate>
+      {isDrafted && (
+        <div className="border border-[#262626] rounded-[8px] fixed left-1/2 top-[90%] transform -translate-x-1/2 z-30 max-w-[890px] w-full flex justify-between items-center px-5">
+          <div className='flex gap-5 items-center'>
+            <div onClick={() => {setIsDrafted(false)}}>
+              <Image
+                className={'cursor-pointer'}
+                width={15}
+                height={15}
+                src={Close}
+                alt="close"
+              />
+            </div>
+            <div>
+              임시 저장된 작업물이 있어요. 저장된 작업을 불러오시겠어요??
+            </div>
+          </div>
+          <div className="w-[90px]">
+            <Button
+              border={ButtonBorder.RECTANGLE}
+              bgColor={BGColor.BLACK}
+              onClick={() => {
+                setIsDraftModal(true);
+              }}
+            >
+              불러오기
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
