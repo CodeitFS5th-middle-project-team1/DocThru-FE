c912aba9a1ee6d7e059a17c7b9d374c78213c5ce
silverraining <www.stella.choi@gmail.com>
2025-04-08 18:04:25 +0900

    날짜 형식 수정 및 그리드로 변경 test

--- src/app/admin/challenges/components/ChallengeTable.tsx
+++ src/app/admin/challenges/components/ChallengeTable.tsx
@@ -1,6 +1,7 @@
 import { Chip } from '@/shared/components/chip/chip';
 import { ApprovalStatusLabels } from '@/types';
 import { useRouter } from 'next/navigation';
+
 interface ChallengeTableProps {
   data: {
     no: number;
@@ -14,48 +15,55 @@ interface ChallengeTableProps {
     id: string;
   }[];
 }
+export const formatDate = (dateString: string) => {
+  if (!dateString) return 'N/A';
 
+  // 날짜 형식 변경
+  const cleaned = dateString.trim().replace(/\s+/g, '').replace(/\.$/, '');
+  return cleaned.replace(/\./g, '/');
+};
 const ChallengeTable = ({ data }: ChallengeTableProps) => {
   const router = useRouter();
 
   return (
     <div className="overflow-x-auto relative shadow-md">
       {/*  헤더 */}
-      <div className="flex justify-between gap-3 md:gap-0 whitespace-nowrap text-[13px] bg-gray-800 text-white py-2 px-3 rounded-lg">
-        <span className="flex-1">No.</span>
-        <span className="flex-[1.2]">분야</span>
-        <span className="flex-[1.3]">카테고리</span>
-        <span className="flex-[3.5]">챌린지 제목</span>
-        <span className="flex-[1.2]">모집 인원</span>
-        <span className="flex-[1.3]">신청일</span>
-        <span className="flex-[1.3]">마감 기한</span>
-        <span className="flex-[1.5]">상태</span>
+      <div className="grid grid-cols-[1fr_1.3fr_1.5fr_3.5fr_2.2fr_1.3fr_1.3fr_1.5fr] text-[13px] bg-custom-gray-800 text-white py-2 px-3 rounded-lg min-w-[640px]">
+        <span>No.</span>
+        <span>분야</span>
+        <span>카테고리</span>
+        <span>챌린지 제목</span>
+        <span>모집 인원</span>
+        <span>신청일</span>
+        <span>마감 기한</span>
+        <span>상태</span>
       </div>
 
       {/* 테이블 */}
-      <table className="w-full">
+      <table className="w-full min-w-[640px]">
         <tbody>
           {data.map((row, index) => (
             <tr
               key={index}
               onClick={() => router.push(`/main/challenge/${row.id}`)}
-              className={`cursor-pointer hover:text-gray-800 hover:bg-indigo-50 transition-colors duration-200
-                flex justify-between whitespace-nowrap border-b border-gray-300 text-[13px] px-3 py-4 text-gray-400 ${
-                  row.status === ApprovalStatusLabels.DELETED
-                    ? 'bg-[#F5f5f5]'
-                    : 'bg-white'
-                }`}
+              className={`grid grid-cols-[1fr_1.3fr_1.5fr_3.5fr_2.2fr_1.3fr_1.3fr_1.5fr] text-[13px] px-3 py-4 border-b border-gray-300 text-gray-400 cursor-pointer hover:text-gray-800 hover:bg-indigo-50 transition-colors duration-200 ${
+                row.status === ApprovalStatusLabels.DELETED
+                  ? 'bg-[#F5f5f5]'
+                  : 'bg-white'
+              }`}
             >
-              <td className="flex-1">{row.no}</td>
-              <td className="flex-[1.2]">{row.type}</td>
-              <td className="flex-[1.3]">{row.category}</td>
-              <td className="flex-[3.5] overflow-hidden text-overflow-ellipsis whitespace-nowrap mr-3">
-                <span>{row.title}</span>
+              <td>{row.no}</td>
+              <td>{row.type}</td>
+              <td>{row.category}</td>
+              <td className="truncate">{row.title}</td>
+              <td>{row.people}</td>
+              <td className="text-center whitespace-pre-line">
+                {formatDate(row.createdAt)}
               </td>
-              <td className="flex-[1.2] pl-2">{row.people}</td>
-              <td className="flex-[1.3]">{row.createdAt}</td>
-              <td className="flex-[1.3]">{row.deadline}</td>
-              <td className="flex-[1.5]">
+              <td className="text-center whitespace-pre-line">
+                {formatDate(row.deadline)}
+              </td>
+              <td>
                 <Chip label={row.status} />
               </td>
             </tr>
