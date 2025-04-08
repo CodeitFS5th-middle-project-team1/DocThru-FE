b046d6ef961078e703c4a3f216bcdbfaabbcf43f
silverraining <www.stella.choi@gmail.com>
2025-04-08 13:19:00 +0900

    design: 삭제된 챌린지 배경 회색 적용, chip whitespace-nowrap 추가

--- src/app/admin/challenges/components/ChallengeTable.tsx
+++ src/app/admin/challenges/components/ChallengeTable.tsx
@@ -1,5 +1,5 @@
 import { Chip } from '@/shared/components/chip/chip';
-
+import { ApprovalStatusLabels } from '@/types';
 interface ChallengeTableProps {
   data: {
     no: number;
@@ -18,15 +18,15 @@ const ChallengeTable = ({ data }: ChallengeTableProps) => {
   return (
     <div className="overflow-x-auto relative shadow-md">
       {/* 테이블 헤더 */}
-      <div className="flex justify-between gap-3 md:gap-0 whitespace-nowrap text-[15px] bg-gray-800 text-white py-2 px-3 rounded-lg">
+      <div className="flex justify-between gap-3 md:gap-0 whitespace-nowrap text-[13px] bg-gray-800 text-white py-2 px-3 rounded-lg">
         <span className="flex-1">No.</span>
         <span className="flex-[1.2]">분야</span>
         <span className="flex-[1.3]">카테고리</span>
-        <span className="flex-5">챌린지 제목</span>
+        <span className="flex-[3.5]">챌린지 제목</span>
         <span className="flex-[1.2]">모집 인원</span>
         <span className="flex-[1.3]">신청일</span>
         <span className="flex-[1.3]">마감 기한</span>
-        <span className="flex-[1.5]">상태</span>
+        <span className="flex-[1.5] ">상태</span>
       </div>
 
       {/* 테이블 본문 */}
@@ -35,18 +35,22 @@ const ChallengeTable = ({ data }: ChallengeTableProps) => {
           {data.map((row, index) => (
             <tr
               key={index}
-              className="flex justify-between whitespace-nowrap bg-white border-b border-gray-400 text-sm px-3 py-4 text-gray-400"
+              className={`flex justify-between whitespace-nowrap border-b border-gray-300 text-[13px] px-3 py-4 text-gray-400 ${
+                row.status === ApprovalStatusLabels.DELETED
+                  ? 'bg-[#F5f5f5]' // 삭제된 챌린지 행에 회색 배경 적용
+                  : 'bg-white'
+              }`}
             >
               <td className="flex-1">{row.no}</td>
               <td className="flex-[1.2]">{row.type}</td>
               <td className="flex-[1.3]">{row.category}</td>
-              <td className="flex-5 overflow-hidden text-overflow-ellipsis whitespace-nowrap mr-3">
+              <td className="flex-[3.5] overflow-hidden text-overflow-ellipsis whitespace-nowrap mr-3">
                 {row.title}
               </td>
               <td className="flex-[1.2] pl-2">{row.people}</td>
               <td className="flex-[1.3]">{row.createdAt}</td>
               <td className="flex-[1.3]">{row.deadline}</td>
-              <td className="flex-[1.5]">
+              <td className="flex-[1.5] ">
                 {/* Chip 컴포넌트에서 이미 매핑 */}
                 <Chip label={row.status} />
               </td>
