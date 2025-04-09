854599526ea8adc47204327a1d0118d5e915e90a
김조순 <whtns611@naver.com>
2025-04-09 22:04:02 +0900

    Feat : api 수정 및 추가

---
+++ src/api/feedback/hook.ts
@@ -0,0 +1,53 @@
+import { useToastQuery } from '@/shared/hooks/useToastQuery';
+import {
+  createFeedBack,
+  fetchFeedBack,
+  FetchFeedBackResponse,
+  patchFeedBack,
+  PatchFeedbackParams,
+} from './api';
+import { useToastMutation } from '@/shared/hooks/useToastMutation';
+import { useQueryClient } from '@tanstack/react-query';
+
+export const useGetFeedBackList = (id: string) => {
+  return useToastQuery<FetchFeedBackResponse, unknown, any>(
+    ['FeedBackList', id],
+    () => fetchFeedBack(id),
+    'FeedBackList-toast'
+  );
+};
+
+export const useCreateFeedBack = (id: string) => {
+  const queryClient = useQueryClient();
+  return useToastMutation<string, FetchFeedBackResponse>(
+    (content) => createFeedBack(id, content),
+    {
+      success: '피드백 생성 성공',
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['FeedBackList', id] });
+      },
+    },
+    'createfeedback-toast'
+  );
+};
+
+export const usePatchFeedBack = (translationId: string) => {
+  const queryClient = useQueryClient();
+  return useToastMutation<PatchFeedbackParams, FetchFeedBackResponse>(
+    ({ feedBackId, content }) =>
+      patchFeedBack(translationId, feedBackId, content),
+    {
+      success: '피드백 수정 성공',
+    },
+    {
+      onSuccess: () => {
+        // queryClient.invalidateQueries({
+        // queryKey: ['FeedBackList', translationId],
+        // });
+      },
+    },
+    'Patcjfeedback-toast'
+  );
+};
