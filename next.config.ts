317486ef06d12649d878d0dc0e67a0ed824858b6
hyuk-dev <hyuk.development@gmail.com>
2025-04-10 05:42:15 +0900

    build 시 out 폴더 생성

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 🔥 build 시 ESLint 에러 무시
   },
+  output: "export", // ✅ 추가: 정적 사이트로 export 가능하게 설정
 };
 
 export default nextConfig;
