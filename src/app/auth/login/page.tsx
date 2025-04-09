7d836447ba89b896112a0c2a44c4454715b3ae92
hyuk-dev <hyuk.development@gmail.com>
2025-04-10 05:58:48 +0900

    타입 에러 해결 시도 login/page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,14 +2,15 @@
 
 import { useLogin } from '@/api/auth/AuthHook';
 import AuthForm from '@/shared/components/form/AuthForm';
+import { AuthFormData } from '@/types';
 import { NextPage } from 'next';
-import { useForm, FormProvider } from 'react-hook-form';
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
 
 const Login: NextPage = () => {
-  const methods = useForm();
+  const methods = useForm<AuthFormData>();
   const login = useLogin();
 
-  const handleSubmit = async (data: any) => {
+  const handleSubmit: SubmitHandler<AuthFormData> = async (data) => {
     login.mutate({
       email: data.email,
       password: data.password,
@@ -21,7 +22,7 @@ const Login: NextPage = () => {
       <FormProvider {...methods}>
         <AuthForm
           category="login"
-          onSubmit={methods.handleSubmit(handleSubmit)}
+          onSubmit={handleSubmit}
           isPending={login.isPending}
         />
       </FormProvider>
--- src/types.ts
+++ src/types.ts
@@ -159,3 +159,8 @@ export type ErrorResponse = {
 
 /** 번역물 생성 및 수정 모달 타입 */
 export type Modal = 'none' | 'drafted' | 'forgive' | 'success' | 'error';
+
+export interface AuthFormData {
+  email: string;
+  password: string;
+}
