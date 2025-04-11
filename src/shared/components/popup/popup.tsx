77094bea33b7fc050ce6caf849b00c14f6535c1c
silverraining <www.stella.choi@gmail.com>
2025-04-12 04:07:56 +0900

    design: 알림 UI, 랜딩페이지 font 라이브러리 설치

--- src/shared/components/popup/popup.tsx
+++ src/shared/components/popup/popup.tsx
@@ -16,33 +16,34 @@ const Popup: React.FC<PopupProps> = ({ isOpen, notifications, onDelete }) => {
   return (
     <div className="absolute right-0 top-6 z-50">
       <div className="relative bg-white rounded-xl border-2 border-custom-gray-300 shadow-lg py-3 m-4 w-90 max-h-115">
-        <div className="flex justify-between items-center pb-2 mb-4">
-          <h3 className="text-lg font-bold px-3">알림</h3>
+        <div className="flex justify-between items-center pb-2 mb-2">
+          <h3 className="text-lg font-bold px-4 ">알림</h3>
         </div>
         <div className="max-h-[390px] overflow-y-auto custom-scrollbar">
           {notifications.length === 0 ? (
-            <p className="text-gray-500">알림이 없습니다.</p>
+            <p className="text-gray-500 px-3 ">알림이 없습니다.</p>
           ) : (
             notifications.map((notification) => (
-              <div key={notification.id} className="px-3 mb-3 border-b pb-2">
+              <div key={notification.id} className="px-3 mb-3 border-b py-2">
                 <p className="text-gray-800">{notification.message}</p>
-
-                <p className="text-gray-400 text-sm mt-2 ml-1">
-                  {new Date(notification.createdAt).toLocaleString('ko-KR', {
-                    year: 'numeric',
-                    month: '2-digit',
-                    day: '2-digit',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: true,
-                  })}{' '}
+                <div className="flex justify-between px-2">
+                  <p className="text-gray-400 text-sm mt-2 ml-1">
+                    {new Date(notification.createdAt).toLocaleString('ko-KR', {
+                      year: 'numeric',
+                      month: '2-digit',
+                      day: '2-digit',
+                      // hour: '2-digit',
+                      // minute: '2-digit',
+                      // hour12: true,
+                    })}{' '}
+                  </p>
                   <button
                     onClick={() => onDelete(notification.id)}
-                    className="underline ml-1 text-[12px] text-gray-500 hover:text-red-500 cursor-pointer"
+                    className="underline ml-1 text-[13px] text-gray-500 hover:text-red-500 cursor-pointer"
                   >
                     삭제
                   </button>
-                </p>
+                </div>
               </div>
             ))
           )}
