439030db230c679e37547720106bd2d40a0cb117
silverraining <www.stella.choi@gmail.com>
2025-04-11 11:35:57 +0900

    fix: accessToken 누락으로 인한 알림 요청 인증 실패 해결

--- src/api/notification/notification.api.ts
+++ src/api/notification/notification.api.ts
@@ -16,12 +16,13 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export async function fetchNotifications(
   userId?: string
 ): Promise<Notification[]> {
-  const url = userId
-    ? `${BASE_URL}/notifications?userId=${userId}` // userId 있으면 쿼리로 붙임
-    : `${BASE_URL}/notifications`; // 없으면 기본 경로 사용
+  const accessToken = localStorage.getItem('accessToken');
+  const url = `${BASE_URL}/notifications?userId=${userId}`;
 
   const res = await fetch(url, {
-    cache: 'no-store',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
     credentials: 'include',
   });
 
@@ -30,8 +31,13 @@ export async function fetchNotifications(
 }
 
 export const deleteNotification = async (id: number) => {
+  const accessToken = localStorage.getItem('accessToken');
+
   const res = await fetch(`${BASE_URL}/notifications/${id}`, {
     method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
     credentials: 'include',
   });
 
