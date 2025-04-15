11e00f8ed391459bd2094e29c2b3c49054a24063
YOONMINHO <ujm4243@naver.com>
2025-04-15 18:40:16 +0900

    feat: 로딩 페이지 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,5 @@
+import AnimateLoad from '@/shared/components/AnimateLoad';
+
 export default function Loading() {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/50">
-      <div className="w-12 h-12 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin" />
-    </div>
-  );
+  return <AnimateLoad />;
 }
